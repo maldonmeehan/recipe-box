0136422c69dc77ceb942f130de9a69635648bf96
Molly McGlone and Maldon Meehan <molly@>
2016-07-21 16:29:06 -0700

    fix integration spec recipe route allows a user to edit the instructions for their recipe

--- spec/intergration_spec.rb
+++ spec/intergration_spec.rb
@@ -49,13 +49,25 @@ describe('the recipe route', :type => :feature) do
     expect(page).to have_content('Jerk Chicken')
   end
 
-  it('allows a use to delete an ingredient') do
+  it('allows a user to delete an ingredient') do
     visit '/'
     fill_in 'name', :with => 'Jerk Chicken'
     click_button 'Go!'
     fill_in 'ingredient', :with => 'Salt'
     click_button 'Add'
     click_button 'Delete'
-    expect(page).to have_no_content('Salt')
+    expect(page).to have_no_content('Saltgit a')
+  end
+
+  it('allows a user to edit the instructions for their recipe') do
+    visit '/'
+    fill_in 'name', :with => 'Jerk Chicken'
+    click_button 'Go!'
+    fill_in 'instruction', :with => 'Wash the chicken, salt (pinch) and let sweat for 10 min. Add pepper and bake at 350 for 2 hours'
+    click_button 'Submit'
+    click_link 'Edit Instructions'
+    fill_in 'instruction', :with => 'Wash chicken, salt, sweat, bake, eat!'
+    click_button 'Update instructions'
+    expect(page).to have_content('Wash chicken, salt, sweat, bake, eat!')
   end
 end
