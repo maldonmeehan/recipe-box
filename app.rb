0eefc7ea8a687b2e831356c282756fdd7520f070
Molly McGlone and Maldon Meehan <molly@>
2016-07-21 10:13:18 -0700

    pass integration spec recipe route, allows the user to name a new recipe

--- app.rb
+++ app.rb
@@ -7,3 +7,34 @@ get ('/') do
   @recipes = Recipe.all()
   erb(:index)
 end
+
+post ('/recipes/new') do
+  name = params.fetch("name")
+  @new_recipe = Recipe.new({:name => name})
+  if @new_recipe.save()
+    redirect("/recipes/".concat(@new_recipe.id().to_s()))
+  else
+    erb(:index)
+  end
+end
+
+get('/recipes/:id') do
+  @recipe = Recipe.find(params.fetch("id").to_i())
+  erb(:recipe_form)
+end
+
+post ('/recipe/:id') do
+  @name = params.fetch('name')
+  ingredient1 = params['ingredient1']
+  ingredient1 = Ingredient.create({:ingredient => ingredient1})
+  ingredient2 = params['ingredient2']
+  ingredient2 = Ingredient.create({:ingredient => ingredient2})
+  ingredient3 = params['ingredient3']
+  ingredient3 = Ingredient.create({:ingredient => ingredient3})
+  ingredient4 = params['ingredient4']
+  ingredient4 = Ingredient.create({:ingredient => ingredient4})
+  ingredient5 = params['ingredient5']
+  ingredient5 = Ingredient.create({:ingredient => ingredient5})
+  @ingredients = Ingredient.all()
+  erb(:recipe)
+end
--- spec/intergration_spec.rb
+++ spec/intergration_spec.rb
@@ -1,8 +1,26 @@
 require('spec_helper')
 
-describe('the home page path', :type => :feature) do
+describe('the home page route', :type => :feature) do
   it "takes us to the home page" do
     visit '/'
     expect(page).to have_content('Welcome')
   end
 end
+
+describe('the recipe route', :type => :feature) do
+  it("allows the user to name a new recipe") do
+    visit '/'
+    fill_in 'name', :with => 'Jerk Chicken'
+    click_button 'Go!'
+    expect(page).to have_content('Jerk Chicken')
+  end
+end
+#     
+#     fill_in 'ingredient1', :with => 'Chicken'
+#     fill_in 'ingredient2', :with => 'Salt'
+#     fill_in 'ingredient3', :with => 'Pepper'
+#     fill_in 'instruction', :with => 'Wash the chicken, salt (pinch) and let sweat for 10 min. Add pepper and bake at 350 for 2 hours'
+#     click_button 'Submit'
+#     expect(page).to have_content('Jerk Chicken')
+#   end
+# end
